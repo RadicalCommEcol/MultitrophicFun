c1c58b3f9ab99c996d4c28f25baf6e1de41d2b19
garciacallejas <david.garcia.callejas@gmail.com>
2022-04-26 13:38:58 +0200

    update phenology overlap function to harmonize column names

--- R/get_phenologic_overlap.R
+++ R/get_phenologic_overlap.R
@@ -3,7 +3,7 @@
 #' returns a numeric matrix, with each element being the overlap
 #' between sp i and sp j relative to the complete phenological period of sp i
 #'
-#' @param sp.data dataframe with the following columns: sp.ID, order, year,
+#' @param sp.data dataframe with the following columns: ID, year,
 #' min.month, min.day, max.month, max.day.
 #' This dataframe can be built from the function 'filter_sp_taxonomy_phenology'
 #'
@@ -11,7 +11,7 @@
 #' @export
 get_phenologic_overlap <- function(sp.data){
   pheno.matrix <- matrix(0,nrow = nrow(sp.data),ncol = nrow(sp.data),
-                         dimnames = list(sp.data$sp.ID,sp.data$sp.ID))
+                         dimnames = list(sp.data$ID,sp.data$ID))
 
   day.start <- as.numeric(strftime(paste(sp.data$year,"-",
                                            sp.data$min.month,"-",
